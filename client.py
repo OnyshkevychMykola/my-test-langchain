266464254bcf05bef795f698eb77fc3e65f0df28
OnyshkevychMykola <mykola.onyshkevych@vitechteam.com>
2026-01-07 14:51:35 +0200

    Added first mcp version

---
+++ client.py
@@ -0,0 +1,34 @@
+import os
+import asyncio
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
+
+from langchain_mcp_adapters.client import MultiServerMCPClient
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+load_dotenv()
+
+mcp_client = MultiServerMCPClient({
+    "tools": {
+        "url": "http://localhost:8000/mcp",
+        "transport": "streamable_http"
+    }
+}
+)
+
+tools = asyncio.run(mcp_client.get_tools())
+
+llm = ChatOpenAI(model="gpt-4o", api_key=OPENAI_API_KEY)
+
+agent = create_agent(llm, tools)
+
+st.title("AI Agent (MCP Version)")
+task = st.text_input("Assign me a task")
+
+
+if task:
+    response = asyncio.run(agent.ainvoke({"messages": task}))
+    st.write(response)
+    final_output = response["messages"][-1].content
+    st.write(final_output)
